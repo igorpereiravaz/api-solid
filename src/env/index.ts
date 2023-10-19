d7a7b356009621e0a36cd4c1c15e05567afc584d
igorpereiravaz <igor.dastan07@gmail.com>
2023-10-19 11:11:17 -0300

    style: :lipstick: refact text

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,9 +9,9 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error('❌ Invalid enviroment variables', _env.error.format())
+  console.error('❌ Invalid environment variables', _env.error.format())
 
-  throw new Error('❌ Invalid enviroment variables')
+  throw new Error('❌ Invalid environment variables')
 }
 
 export const env = _env.data
