4c42e97495012b77ae33487fa74e3570b1f0170d
igorpereiravaz <igor.dastan07@gmail.com>
2023-10-23 11:58:46 -0300

    refactor: :lipstick: refact register using controller

---
+++ src/http/routes.ts
@@ -0,0 +1,6 @@
+import fastify, { FastifyInstance } from 'fastify'
+import { register } from './controllers/register'
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/users', register)
+}
