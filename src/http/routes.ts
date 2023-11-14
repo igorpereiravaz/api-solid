7815188a227fadcc53af9669538e0d35a2fdb330
igorpereiravaz <igor.dastan07@gmail.com>
2023-11-14 11:04:24 -0300

    feat: :sparkles: implement controller and test for authenticate method

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
-import fastify, { FastifyInstance } from 'fastify'
+import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
+import { authenticate } from './controllers/authenticate'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+
+  app.post('/sessions', authenticate)
 }
